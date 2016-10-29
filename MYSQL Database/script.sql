3703173f3211221f47ce1319f6930a49ca9037e4
user <jpooch58@gmail.com>
2016-10-29 16:11:45 -0600

    Updated Script

--- MYSQL Database/script.sql
+++ MYSQL Database/script.sql
@@ -1,11 +1,11 @@
 CREATE DATABASE College_Majors;
 USE College_Majors;
 
-CREATE TABLE All_Majors(Ranking INT, DegreeType char(255), Major char(50), EarlyPay char(15), MidPay char(15));
-CREATE TABLE Arts_and_Science(Ranking INT(255), DegreeType char(255), Major char(50), EarlyPay char(15), MidPay char(15));
-CREATE TABLE Business(Ranking INT(255), DegreeType char(255), Major char(50), EarlyPay char(15), MidPay char(15));
-CREATE TABLE Education(Ranking INT(255), DegreeType char(255), Major char(50), EarlyPay char(15), MidPay char(15));
-CREATE TABLE Engineering(Ranking INT(255), DegreeType char(255), Major char(50), EarlyPay char(15), MidPay char(15));
+CREATE TABLE All_Majors(Ranking INT, Major char(255), DegreeType char(50), EarlyPay char(15), MidPay char(15));
+CREATE TABLE Arts_and_Science(Ranking INT(255), Major char(255), DegreeType char(50), EarlyPay char(15), MidPay char(15));
+CREATE TABLE Business(Ranking INT(255), Major char(255), DegreeType char(50), EarlyPay char(15), MidPay char(15));
+CREATE TABLE Education(Ranking INT(255), Major char(255), DegreeType char(50), EarlyPay char(15), MidPay char(15));
+CREATE TABLE Engineering(Ranking INT(255), Major char(255), DegreeType char(50), EarlyPay char(15), MidPay char(15));
 
 LOAD DATA LOCAL INFILE 'All_Majors.txt' INTO TABLE All_Majors LINES TERMINATED BY '\r';
 LOAD DATA LOCAL INFILE 'AandS.txt' INTO TABLE Arts_and_Science LINES TERMINATED BY '\r';
